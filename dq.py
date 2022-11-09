cbb1eb0f5e2996452aa66150256f14157241b80a
vcode3 <94299377+vcode3@users.noreply.github.com>
2022-11-10 00:37:18 +0100

    Update dq.py

--- dq.py
+++ dq.py
@@ -1,1 +1,15 @@
 # dq.py
+
+
+df1 = pd.read_csv('$path/dq1.csv')
+
+saved_column = df.city 
+
+print (saved_column)
+
+df2 = pd.read_csv('D:/vaidehi-learnings/python/dq/domain_city.csv')
+domainCity = df1.city #you can also use df['column_name']
+print (domainCity)
+
+isCityValid = df1.city.isin(df2.city).astype(str) 
+isCityValid.to_csv('D:/vaidehi-learnings/python/dq/domain_error.csv')
