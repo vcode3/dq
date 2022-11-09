d94b6cd534de61339090208471f98d8717cdb100
vcode3 <94299377+vcode3@users.noreply.github.com>
2022-11-10 00:53:37 +0100

    Update dq.py

--- dq.py
+++ dq.py
@@ -4,8 +4,6 @@ import pandas as pd
 df1 = pd.read_csv('$path/dq1.csv')
 
 df2 = pd.read_csv('$path/domain_city.csv')
-domainCity = df1.city 
-
 
 isCityValid = df1.city.isin(df2.city).astype(str) 
 isCityValid.to_csv('$path/domain_error.csv')
